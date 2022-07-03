ff015cb937ff5da04225cdf8fc85c61dc49d9930
Devesh Sharma <deveshsharma1009@gmail.com>
2022-07-03 16:41:35 +0530

    added signup, login, add friend, get friend, add post, get post, get image file api's

--- MongoController.py
+++ MongoController.py
@@ -23,21 +23,21 @@ information.insert_one(user_schema)
 class MongoController:
     def __init__(self, uri, db_name, collection_name, admin_name, admin_passwd):
         """
-        Usage:  Constructor to set all the value of this class
+        ``Usage``:  Constructor to set all the value of this class
 
-        RETURNS : None
+        ``RETURNS`` : None
 
-        URI : it is the url to connect to mongodb e.g localhost:2000/
+        ``URI`` : it is the url to connect to mongodb e.g localhost:2000/
 
-        DB_NAME : it is the name of database you want to use
+        ``DB_NAME`` : it is the name of database you want to use
 
-        COLLECTION_NAME : it is the name of collection you want to use
+        ``COLLECTION_NAME`` : it is the name of collection you want to use
 
-        ADMIN_NAME : name of the administrator which can see all the records of database
+        ``ADMIN_NAME`` : name of the administrator which can see all the records of database
 
-        ADMIN_PASSWD : password for authorizing the admin
+        ``ADMIN_PASSWD`` : password for authorizing the admin
 
-        NOTE: make sure to use environment variables for setting value in  this field
+        ``NOTE``: make sure to use environment variables for setting value in  this field
         """
         self.uri = uri
         self.db_name = db_name
@@ -45,39 +45,41 @@ class MongoController:
         self.admin_name = admin_name
         self.admin_passwd = admin_passwd
         self.start = 0
+        self.post_start = 0
+        self.current_user = 0
 
     def change_collection(self, new_collection_name):
         """
-        Usage: This function help in changing the value of collection_name and switching collection
+        ``Usage``: This function help in changing the value of collection_name and switching collection
 
-        RETURNS : None
+        ``RETURNS`` : None
 
-        NEW_COLLECTION_NAME: new name of collection you are working with
+        ``NEW_COLLECTION_NAME``: new name of collection you are working with
         """
         self.collection_name = new_collection_name
         self.collection = self.db.get_collection(new_collection_name)
 
     def connect_to_mongo(self):
         """
-        Usage: This function connects to database
+        ``Usage``: This function connects to database
 
-        RETURNS : None
+        ``RETURNS`` : None
         """
         self.client = mongo_client.MongoClient(self.uri)
         self.db = self.client.get_database(self.db_name)
 
     def set_collection(self):
         """
-        Usage : This function sets the value of collection for the class
+        ``Usage`` : This function sets the value of collection for the class
         """
         self.connect_to_mongo()
         self.collection = self.db.get_collection(self.collection_name)
 
     def insert_data_in_collection(self, data):
         """
-        Usage : It is used to add new data to collection without checking if it already exists.
+        ``Usage`` : It is used to add new data to collection without checking if it already exists.
 
-        RETURNS : None
+        ``RETURNS`` : None
         """
         self.set_collection()
         is_inserted = self.collection.insert_one(data).acknowledged
@@ -85,13 +87,13 @@ class MongoController:
 
     def insert_unique_data_in_collection(self, data, key=None):
         """
-        Usage : It is used to add new data to collection if it already exists.
+        ``Usage`` : It is used to add new data to collection if it already exists.
 
-        DATA : data to be inserted
+        ``DATA`` : data to be inserted
 
-        KEY : value to identifty unique elements
+        ``KEY`` : value to identifty unique elements
 
-        RETURNS : True if data is inserted else returns False if the given data already exists.
+        ``RETURNS`` : True if data is inserted else returns False if the given data already exists.
         """
         if key == None:
             key = data
@@ -118,13 +120,13 @@ class MongoController:
 
     def delete_data_in_collection(self, data, key=None):
         """
-        Usage : It is used to delete data from collection.
+        ``Usage`` : It is used to delete data from collection.
 
-        DATA : data to be deleted
+        ``DATA`` : data to be deleted
 
-        KEY : value to identifty unique elements
+        ``KEY`` : value to identifty unique elements
 
-        RETURNS : True if data is deleted data sucessfully else returns False no such data exists.
+        ``RETURNS`` : True if data is deleted data sucessfully else returns False no such data exists.
         """
         if key == None:
             key = data
@@ -137,9 +139,9 @@ class MongoController:
 
     def search(self, data):
         """
-        Usage : It returns the data from mongodb if its member match with data given
+        ``Usage`` : It returns the data from mongodb if its member match with data given
 
-        DATA : it is data to search in database
+        ``DATA`` : it is data to search in database
         """
         self.set_collection()
         result = self.collection.find_one(data)
@@ -159,14 +161,11 @@ class MongoController:
         try:
             result = result[self.start:self.start+count]
             self.start += count
-            print(result)
-            print(self.start)
             if self.start > len(result):
                 self.start = 0
             return result
             
         except Exception as e:
-            print(e)
             return result
 
     def search_collection(self,collection_name):
@@ -179,13 +178,34 @@ class MongoController:
     def create_collection(self,collection_name,initial_data={"name":""}):
         self.change_collection(collection_name)
         self.insert_data_in_collection(initial_data)
-
-# creates a collection in database
-# db = MongoController("mongodb://localhost:27017","JNV_Media","user_details","root","")
-# db.connect_to_mongo()
-# db.create_collection("devesh",{
-#     "name":"devesh"
-# })
-
-# checks if collection exists in  the database
-# print(db.search_collection("deveshs"))
+        
+    def get_all_collection_starts_with(self,identifier,separator="-"):
+        self.all_collections = self.db.list_collection_names()
+        result = []
+        for collection in self.all_collections:
+            if collection.split(separator)[0] == identifier:
+                result.append(collection)
+        return result
+    def get_some_posts(self,hide_fields=["_id","password","name"]):
+        result = self.get_all_collection_starts_with("posts","-")
+        if self.current_user >= len(result):
+            self.current_user = 0
+        self.change_collection(result[self.current_user])
+        self.set_collection()
+        posts = []
+        self.current_user += 1
+        for x in self.collection.find():
+            required = {}
+            for key in x:
+                if key in hide_fields:
+                    continue
+                else:
+                    required[key] = x[key]
+            posts.append(required)
+        posts_not_empty = []
+        for i in posts:
+            if i == {}:
+                continue
+            posts_not_empty.append(i)
+        return posts_not_empty
+        
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-import json # to convert json file object into python dictionary
-import os
-import re # to save image files and running system commands
-from flask import Flask, jsonify, request # to make server and send json as response and to get values from request parameters
-from MongoController import MongoController # to control mongodb
-from flask_cors import CORS # allow cross origin
-import hashlib # for hashing passwords into hash
-
+import json  # to convert json file object into python dictionary
+import os  # to save image files and running system commands
+from flask import (
+    Flask,
+    jsonify,
+    request,
+    send_file,
+)  # to make server and send json as response and to get values from request parameters
+from MongoController import MongoController  # to control mongodb
+from flask_cors import CORS  # allow cross origin
+import hashlib  # for hashing passwords into hash
 
 
 # json to store variables in json file
-__variable_json_path__ = r"D:\Github Projects\social_media_website_backend\secret_variables.json"
+__variable_json_path__ = (
+    r"D:\Github Projects\social_media_website_backend\secret_variables.json"
+)
 
 # reads file
-file = open(__variable_json_path__,"r")
+file = open(__variable_json_path__, "r")
 
 # converts json file into python dictionary
 variables = json.load(file)
@@ -21,234 +26,310 @@ variables = json.load(file)
 app = Flask(__name__)
 
 # configures upload folder from json file folder location
-app.config['UPLOAD_FOLDER'] = variables["UPLOAD_FOLDER"]
+app.config["UPLOAD_FOLDER"] = variables["UPLOAD_FOLDER"]
 
 # allows cross origin
 CORS(app)
 
 # class to manage mongodb database
-db = MongoController(variables["db_uri"], variables["db_name"],variables["collection_name"],variables["admin_name"],variables["admin_password"])
+db = MongoController(
+    variables["db_uri"],
+    variables["db_name"],
+    variables["collection_name"],
+    variables["admin_name"],
+    variables["admin_password"],
+)
 db.set_collection()
 
 
 # checks if given filename is in allowed list
-def allowed_file(filename,allowed_extension_list=variables["ALLOWED_EXTENSIONS"]):
+def allowed_file(filename, allowed_extension_list=variables["ALLOWED_EXTENSIONS"]):
     """
-    Usage : checks if given filename is in allowed list
-    
-    FILENAME : name of file to check if it has allowed extension
+    ``Usage`` : checks if given filename is in allowed list
+
+    ``FILENAME`` : name of file to check if it has allowed extension
     """
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in allowed_extension_list
+    return (
+        "." in filename and filename.rsplit(".", 1)[1].lower() in allowed_extension_list
+    )
 
 
-def secure_filename(email,file_extension="png"):
+def secure_filename(email, file_extension="png"):
     """
-    Usage : It returns the secure name of file
-    
-    EMAIL : it is the email of the user to save file
-    
-    FILE_EXTENSION : extension of file to save
+    ``Usage`` : It returns the secure name of file
+
+    ``EMAIL`` : it is the email of the user to save file
+
+    ``FILE_EXTENSION`` : extension of file to save
     """
     return f"avatar.{email}.{file_extension}"
 
 
-def secure_filename_post(title,email,file_extension="png"):
+def secure_filename_post(title, email, file_extension="png"):
     """
-    Usage : It returns the secure name of file
-    
-    TITLE : title of the post
-    
-    EMAIL : it is the email of the user to save file
-    
-    FILE_EXTENSION : extension of file to save
+    ``Usage`` : It returns the secure name of file
+
+    ``TITLE`` : title of the post
+
+    ``EMAIL`` : it is the email of the user to save file
+
+    ``FILE_EXTENSION`` : extension of file to save
     """
-    
+
     title = title.split(" ")
-    title = ''.join(title)
+    title = "".join(title)
     return f"post_file.{title}.{email}.{file_extension}"
 
 
-@app.route("/login",methods=['GET', 'POST'])
+def get_extension(filename):
+    """
+    ``Usage`` : It gives the extension of file
+
+    ``FILENAME`` : it is the name of file for which you want its extension
+    """
+    return filename.split(".")[len(filename.split(".")) - 1]
+
+
+@app.route("/login", methods=["GET", "POST"])
 def Login():
     """
-    Usage : It is the API for login the user through only post request and stores its information in mongodb
+    ``Usage`` : It is the API for login the user through only post request and stores its information in mongodb
     """
     if request.method == "POST":
         data = request.get_json()
         email = data["email"]
         password = data["password"]
         if len(password) < 8:
-            return jsonify({
-                "sucess":False,
-                "message":"It must be at least 8 digits",
-                "title":"Password is too short"
-            })
+            return jsonify(
+                {
+                    "sucess": False,
+                    "message": "It must be at least 8 digits",
+                    "title": "Password is too short",
+                }
+            )
         hash = hashlib.sha256()
         hash.update(password.encode())
         hash = hash.hexdigest()
-        result = db.search({"email":email,"password":hash})
+        db.change_collection("user_details")
+        db.set_collection()
+        result = db.search({"email": email, "password": hash})
         if result:
             username = result["username"]
             password = result["password"]
-            print(username,password)
-            return jsonify({
-                "sucess":True,
-                "message":"Login Sucessfully.",
-                "Api_Key":password,
-                "User_Name":username,
-                "email":email}
-                )
+            avatar = result["avatar"]
+            return jsonify(
+                {
+                    "sucess": True,
+                    "message": "Login Sucessfully.",
+                    "Api_Key": password,
+                    "User_Name": username,
+                    "email": email,
+                    "avatar": avatar,
+                }
+            )
         else:
             return jsonify(
-                {"sucess":False,
-                 "message":"Please enter correct email or password"
-                })
+                {"sucess": False, "message": "Please enter correct email or password"}
+            )
     else:
-        return jsonify({
-            "error":"This method is not allowed."
-        })
+        return jsonify({"error": "This method is not allowed."})
 
-@app.route("/signup",methods=['GET', 'POST'])
+
+@app.route("/signup", methods=["GET", "POST"])
 def SignUp():
     if request.method == "POST":
         data = request.form
-        print(data)
+        db.change_collection("user_details")
+        db.set_collection()
         username = data["username"]
         email = data["email"]
         mobile_number = data["mobile_number"]
         if len(mobile_number) < 8:
-            return jsonify({
-                "sucess":False,
-                "message":"mobile number must be 10 digits",
-                "title":"Invalid Mobile Number"
-            })
+            return jsonify(
+                {
+                    "sucess": False,
+                    "message": "mobile number must be 10 digits",
+                    "title": "Invalid Mobile Number",
+                }
+            )
         password = data["password"]
         if len(password) < 8:
-            return jsonify({
-                "sucess":False,
-                "message":"It must be at least 8 digits",
-                "title":"Password is too short"
-            })
+            return jsonify(
+                {
+                    "sucess": False,
+                    "message": "It must be at least 8 digits",
+                    "title": "Password is too short",
+                }
+            )
         avatar = request.files["userimage"]
-        if avatar.filename == '':
-            print('No selected file')
+        if avatar.filename == "":
             return "No file selected"
 
         hash = hashlib.sha256()
         hash.update(password.encode())
         hash = hash.hexdigest()
         if avatar and allowed_file(avatar.filename):
-            # filename = secure_filename(email)
-            db.insert_unique_data_in_collection({
-                "username":username,
-                "email":email,
-                "mobile_number":mobile_number,
-                "password":hash,
-                "avatar":avatar
-            },"email")
-            return jsonify({"sucess":True,"message":"Account created sucessfully.","Api_Key":hash,"User_Name":username,"email":email,"avatar":avatar})
-            
+            db.insert_unique_data_in_collection(
+                {
+                    "username": username,
+                    "email": email,
+                    "mobile_number": mobile_number,
+                    "password": hash,
+                    "avatar": secure_filename(email, get_extension(avatar.filename)),
+                },
+                "email",
+            )
+            os.mkdir(app.config["UPLOAD_FOLDER"] + "\\avatar" "\\" + email + "\\")
+            avatar.save(
+                os.path.join(
+                    app.config["UPLOAD_FOLDER"] + "\\avatar" "\\" + email + "\\",
+                    secure_filename(email, get_extension(avatar.filename)),
+                )
+            )
+            return jsonify(
+                {
+                    "sucess": True,
+                    "message": "Account created sucessfully.",
+                    "Api_Key": hash,
+                    "User_Name": username,
+                    "email": email,
+                    "avatar": secure_filename(email, get_extension(avatar.filename)),
+                }
+            )
+
     else:
-        return jsonify({
-            "error":"This method is not allowed."
-        })
-   
-@app.route("/getfriends",methods=["GET","POST"])
-def GetFriends():
-    if request.method == "POST":
-        data = request.get_json()
-        email = data["email"]
-        api_key = data["api_key"]
-        count = 10
-        result = db.search({"password":api_key,"email":email})
-        dbemail = result["email"]
-        print(dbemail,email)
-        if email == dbemail:
-            users = db.get_some_data(count,["password","mobile_number","_id"])
-            return jsonify({"sucess":True,
-                            "message":"User found",
-                            "friends" : users})
-        else:
-            return jsonify({"sucess":False,
-                            "message":"Invalid user details",
-                            "title":"No such user"})
-    else:
-        return jsonify({
-            "error":"This method is not allowed."
-        })
+        return jsonify({"error": "This method is not allowed."})
 
 
-@app.route("/add-post",methods=["GET",'POST'])
-def AddPost():
-    if request.method == "POST":
-        if 'post_file' not in request.files:
-            print("No file.")
-            return "No file"
-        file = request.files["post_file"]
-        print(file)
-        print(request.form)
-        email = request.form["email"]
-        api_key = request.form["api_key"]
-        post_title = request.form["post_title"]
-        post_desc = request.form["post_desc"]
-        print(file.filename)
-        if file.filename == '':
-            print('No selected file')
-            return jsonify({"sucess":False,"message":"Invalid user"})
-
-        if file and allowed_file(file.filename):
-            filename = secure_filename_post(post_title,email)
-            info = {
-                "password":api_key,
-                "email":email,
-                "post_file":secure_filename_post(post_title,email)
-            }
-            # db.insert_unique_data_in_collection(info,"post_file")
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return jsonify({"sucess":False,"message":"Invalid user"})
-        else:
-            return jsonify({"sucess":False,"message":"Invalid user"})
-            
-    else:
-        print("Wrong Mehtod")
-        return jsonify({"sucess":False,"message":"Invalid user"})
-
-@app.route("/add-friend",methods=["GET","POST"])
+@app.route("/add-friend", methods=["GET", "POST"])
 def AddFriend():
     if request.method == "POST":
         data = request.get_json()
-        print(data)
+        db.change_collection("user_details")
+        db.set_collection()
         email = data["email"]
         friend_email = data["friend_email"]
         api_key = data["api_key"]
-        info = {"email":email,
-                "password":api_key}
+        info = {"email": email, "password": api_key}
         result = db.search(info)
-        print(result)
         if result:
-            print(result)
-            print("returning 3")
             collection_exists = db.search_collection(f"friends-{email}")
-            print("returning 1")
-            print(collection_exists)
-            print("returning 2")
             if collection_exists:
                 db.change_collection(f"friends-{email}")
                 db.set_collection()
-                db.insert_unique_data_in_collection({"friend_email":friend_email})
-                print("returning 4")
-                return jsonify({"success":True})
+                db.insert_unique_data_in_collection({"friend_email": friend_email})
+                return jsonify({"success": True})
             else:
                 db.create_collection(f"friends-{email}")
-                db.insert_unique_data_in_collection({"friend_email":friend_email})
-                return jsonify({"success":True})
+                db.insert_unique_data_in_collection({"friend_email": friend_email})
+                return jsonify({"success": True})
 
     else:
         return "Wrong Method"
 
 
-    
+@app.route("/getfriends", methods=["GET", "POST"])
+def GetFriends():
+    if request.method == "POST":
+        data = request.get_json()
+        email = data["email"]
+        api_key = data["api_key"]
+        count = 30
+        db.change_collection("user_details")
+        db.set_collection()
+        result = db.search({"password": api_key, "email": email})
+        dbemail = result["email"]
+        if email == dbemail:
+            users = db.get_some_data(count, ["password", "mobile_number", "_id"])
+            return jsonify({"sucess": True, "message": "User found", "friends": users})
+        else:
+            return jsonify(
+                {
+                    "sucess": False,
+                    "message": "Invalid user details",
+                    "title": "No such user",
+                }
+            )
+    else:
+        return jsonify({"error": "This method is not allowed."})
+
+
+@app.route("/add-post", methods=["GET", "POST"])
+def AddPost():
+    if request.method == "POST":
+        if "post_file" not in request.files:
+            return "No file"
+        file = request.files["post_file"]
+        email = request.form["email"]
+        api_key = request.form["api_key"]
+        post_title = request.form["post_title"]
+        post_desc = request.form["post_desc"]
+        if file.filename == "":
+            return jsonify({"sucess": False, "message": "No file selected"})
+
+        if file:
+            filename = secure_filename_post(
+                post_title, email, get_extension(file.filename)
+            )
+            info = {
+                "password": api_key,
+                "email": email,
+                "post_file": filename,
+                "post_title": post_title,
+                "post_desc": post_desc,
+            }
+            if not db.search_collection("posts-" + email):
+                db.create_collection("posts-" + email)
+
+            db.change_collection("posts-" + email)
+            db.set_collection()
+            db.insert_unique_data_in_collection(info)
+            try:
+                os.mkdir(app.config["UPLOAD_FOLDER"] + "\\post_files" "\\" + email)
+            except Exception as e:
+                print(e)
+            finally:
+                file.save(
+                    os.path.join(
+                        app.config["UPLOAD_FOLDER"] + "\\post_files"
+                        "\\" + email + "\\",
+                        filename,
+                    )
+                )
+                return jsonify({"sucess": True})
+        else:
+            return jsonify({"sucess": False, "message": "Invalid user"})
+
+    else:
+        return jsonify({"sucess": False, "message": "Invalid user"})
+
+
+@app.route("/get_posts", methods=["GET", "POST"])
+def GetPosts():
+    if request.method == "POST":
+        data = request.get_json()
+        email = data["email"]
+        api_key = data["api_key"]
+        info = {
+            "password": api_key,
+            "email": email,
+        }
+        if db.search(info):
+            posts_collection = db.get_some_posts()
+            return jsonify({"sucess": True, "posts": posts_collection})
+    else:
+        return jsonify({"sucess": False, "message": "Invalid user"})
+
+
+@app.route("/get_image_file")
+def get_image_file():
+    arguments = request.args
+    email = arguments["email"]
+    file_name = arguments["file_name"]
+    directory = arguments["image_category"]
+    file_dir = f"{os.getcwd()}\\upload\\{directory}\\{email}\\{file_name}"
+    return send_file(file_dir, mimetype="image/jpg")
+
 
 if __name__ == "__main__":
-    app.run(debug=True,port=2500)
+    app.run(debug=True, port=2500)
